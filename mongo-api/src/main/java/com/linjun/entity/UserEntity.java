486d97050227aaadb9ed43f62b191b28456ef931
linjun <1045735402@qq.com>
2018-03-13 21:51:06 +0800

    公共模块的搭建 管理模块的  api逻辑层

---
+++ mongo-api/src/main/java/com/linjun/entity/UserEntity.java
@@ -0,0 +1,174 @@
+package com.linjun.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author 林俊
+ * @create 2018/3/13.
+ * @desc
+ **/
+public class UserEntity implements Serializable {
+    private Long userId;
+    private String userName;
+    private  String password;
+    private  String gender;
+    private Date birthday;
+    private  String school;
+    private  String major;
+    private  Integer infoFull;
+    private  Date register_time;
+    private  Date last_login_time;
+    private  String last_login_ip;
+    private  Long user_level_id;
+    private  String nickname;
+    private  String mobile;
+    private  String register_ip;
+    private  String avatar;
+    private  String weixin_openid;
+    private  Long parent_id;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public Integer getInfoFull() {
+        return infoFull;
+    }
+
+    public void setInfoFull(Integer infoFull) {
+        this.infoFull = infoFull;
+    }
+
+    public Date getRegister_time() {
+        return register_time;
+    }
+
+    public void setRegister_time(Date register_time) {
+        this.register_time = register_time;
+    }
+
+    public Date getLast_login_time() {
+        return last_login_time;
+    }
+
+    public void setLast_login_time(Date last_login_time) {
+        this.last_login_time = last_login_time;
+    }
+
+    public String getLast_login_ip() {
+        return last_login_ip;
+    }
+
+    public void setLast_login_ip(String last_login_ip) {
+        this.last_login_ip = last_login_ip;
+    }
+
+    public Long getUser_level_id() {
+        return user_level_id;
+    }
+
+    public void setUser_level_id(Long user_level_id) {
+        this.user_level_id = user_level_id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getRegister_ip() {
+        return register_ip;
+    }
+
+    public void setRegister_ip(String register_ip) {
+        this.register_ip = register_ip;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getWeixin_openid() {
+        return weixin_openid;
+    }
+
+    public void setWeixin_openid(String weixin_openid) {
+        this.weixin_openid = weixin_openid;
+    }
+
+    public Long getParent_id() {
+        return parent_id;
+    }
+
+    public void setParent_id(Long parent_id) {
+        this.parent_id = parent_id;
+    }
+}
