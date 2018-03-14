83d2ca2d027fbb7fb4920cc7079fbbfe48ea0d3a
linjun <1045735402@qq.com>
2018-03-14 10:37:37 +0800

    公共模块的搭建 API模块 第三方登入qqmap表

---
+++ mongo-api/src/main/java/com/linjun/dao/ApiCodeMapper.java
@@ -0,0 +1,14 @@
+package com.linjun.dao;
+
+import com.linjun.entity.CodeEntity;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author 林俊
+ * @create 2018/3/14.
+ * @desc
+ **/
+public interface ApiCodeMapper extends  BaseDao<CodeEntity> {
+    CodeEntity queryByMail(@Param("email")String email);
+    CodeEntity queryByMobile(@Param("mobile")String mobile) ;
+}
---
+++ mongo-api/src/main/java/com/linjun/dao/ApiUserQQMapper.java
@@ -0,0 +1,11 @@
+package com.linjun.dao;
+
+import com.linjun.entity.UserQQEntity;
+
+/**
+ * @author 林俊
+ * @create 2018/3/14.
+ * @desc
+ **/
+public interface ApiUserQQMapper extends BaseDao<UserQQEntity> {
+}
