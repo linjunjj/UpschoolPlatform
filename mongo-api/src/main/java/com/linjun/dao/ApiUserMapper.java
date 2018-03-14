10cba3cf0b5fca33a896595a98b4677dc6b33a54
linjun <1045735402@qq.com>
2018-03-14 08:32:17 +0800

    公共模块的搭建 API模块 用户表

---
+++ mongo-api/src/main/java/com/linjun/dao/ApiUserMapper.java
@@ -0,0 +1,18 @@
+package com.linjun.dao;
+
+import com.linjun.entity.SmsLogVo;
+import com.linjun.entity.UserEntity;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author 林俊
+ * @create 2018/3/14.
+ * @desc
+ **/
+public interface ApiUserMapper extends  BaseDao<UserEntity> {
+    UserEntity queryByMobile(@Param(("mobile")) String mobile);
+    UserEntity queryByOpenId(@Param("openId") String openId);
+    SmsLogVo querySmsCodeByUserId(@Param("user_id") Long user_id);
+    int saveSmsCodeLog(SmsLogVo smsLogVo);
+
+}
