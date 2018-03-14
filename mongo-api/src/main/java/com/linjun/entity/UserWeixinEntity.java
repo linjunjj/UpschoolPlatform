6561b78deb232c4d91e7d438a69a64f2760ec4fb
linjun <1045735402@qq.com>
2018-03-14 10:44:06 +0800

    公共模块的搭建 API模块 第三方登入weixinmap表

---
+++ mongo-api/src/main/java/com/linjun/entity/UserWeixinEntity.java
@@ -0,0 +1,115 @@
+package com.linjun.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author 林俊
+ * @create 2018/3/14.
+ * @desc
+ **/
+public class UserWeixinEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+     private Long id;
+     private  Long userId;
+     private String openid;
+     private  String nickname;
+     private  String sex;
+     private  String province;
+     private String city;
+     private String country;
+     private String headimgurl;
+     private  String privilege;
+     private String unionid;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getHeadimgurl() {
+        return headimgurl;
+    }
+
+    public void setHeadimgurl(String headimgurl) {
+        this.headimgurl = headimgurl;
+    }
+
+    public String getPrivilege() {
+        return privilege;
+    }
+
+    public void setPrivilege(String privilege) {
+        this.privilege = privilege;
+    }
+
+    public String getUnionid() {
+        return unionid;
+    }
+
+    public void setUnionid(String unionid) {
+        this.unionid = unionid;
+    }
+}
