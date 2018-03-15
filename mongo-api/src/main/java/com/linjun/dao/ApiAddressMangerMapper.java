7175fca712a832d3ee9bce80f8cf1d330f72ac41
linjun <1045735402@qq.com>
2018-03-15 11:48:22 +0800

    公共模块的搭建 API模块  orm层

---
+++ mongo-api/src/main/java/com/linjun/dao/ApiAddressMangerMapper.java
@@ -0,0 +1,11 @@
+package com.linjun.dao;
+
+import com.linjun.entity.AddressManger;
+
+/**
+ * @author 林俊
+ * @create 2018/3/15.
+ * @desc
+ **/
+public interface ApiAddressMangerMapper extends BaseDao<AddressManger> {
+}
