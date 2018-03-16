1efd38c44ce4496090d8807e093de0bc420f5ad4
linjun <1045735402@qq.com>
2018-03-16 09:30:36 +0800

    公共模块的搭建 API模块  订单

---
+++ mongo-api/src/main/java/com/linjun/dao/ApiUserOrderMapper.java
@@ -0,0 +1,11 @@
+package com.linjun.dao;
+
+import com.linjun.entity.UserOrderEntity;
+
+/**
+ * @author 林俊
+ * @create 2018/3/16.
+ * @desc
+ **/
+public interface ApiUserOrderMapper extends  BaseDao<UserOrderEntity>{
+}
