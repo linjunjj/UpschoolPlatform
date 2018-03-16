100e1c7bf6a1f02bd313157db368748323dbba39
linjun <1045735402@qq.com>
2018-03-16 08:28:06 +0800

    公共模块的搭建 API模块  收入

---
+++ mongo-api/src/main/java/com/linjun/dao/ApiUserIncomeMapper.java
@@ -0,0 +1,11 @@
+package com.linjun.dao;
+
+import com.linjun.entity.UserIncomeEntity;
+
+/**
+ * @author 林俊
+ * @create 2018/3/15.
+ * @desc
+ **/
+public interface ApiUserIncomeMapper extends BaseDao<UserIncomeEntity> {
+}
