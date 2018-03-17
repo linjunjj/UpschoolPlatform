54fc56d998362faf19deda9dabee5f0a57fc85ac
linjun <1045735402@qq.com>
2018-03-17 19:56:12 +0800

    公共模块的搭建 API模块  广告

---
+++ mongo-api/src/main/java/com/linjun/dao/ApiAdMapper.java
@@ -0,0 +1,11 @@
+package com.linjun.dao;
+
+import com.linjun.entity.AdEntity;
+
+/**
+ * @author 林俊
+ * @create 2018/3/17.
+ * @desc
+ **/
+public interface ApiAdMapper extends  BaseDao<AdEntity> {
+}
