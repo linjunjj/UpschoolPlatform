ffee750537bc8798fd2252582addb357eb77db74
linjun <1045735402@qq.com>
2018-03-17 20:12:43 +0800

    公共模块的搭建 API模块  引导页面

---
+++ mongo-api/src/main/java/com/linjun/dao/ApiChannelMapper.java
@@ -0,0 +1,11 @@
+package com.linjun.dao;
+
+import com.linjun.entity.ChannelEntity;
+
+/**
+ * @author 林俊
+ * @create 2018/3/17.
+ * @desc
+ **/
+public interface ApiChannelMapper extends  BaseDao<ChannelEntity>{
+}
