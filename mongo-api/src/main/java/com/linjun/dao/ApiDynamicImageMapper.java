613f0d4a98888fac86c18167e596dbb1529fd29d
linjun <1045735402@qq.com>
2018-03-17 21:02:04 +0800

    公共模块的搭建 API模块  修改部分dynamic模块

---
+++ mongo-api/src/main/java/com/linjun/dao/ApiDynamicImageMapper.java
@@ -0,0 +1,11 @@
+package com.linjun.dao;
+
+import com.linjun.entity.DynamicImageEntity;
+
+/**
+ * @author 林俊
+ * @create 2018/3/17.
+ * @desc
+ **/
+public interface ApiDynamicImageMapper extends  BaseDao<DynamicImageEntity> {
+}
