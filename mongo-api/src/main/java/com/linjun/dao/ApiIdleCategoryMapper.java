3a088fa9d348e5ebefdfa11057711e58d8b289b3
linjun <1045735402@qq.com>
2018-03-18 14:17:43 +0800

    公共模块的搭建 API模块  闲置

---
+++ mongo-api/src/main/java/com/linjun/dao/ApiIdleCategoryMapper.java
@@ -0,0 +1,11 @@
+package com.linjun.dao;
+
+import com.linjun.entity.IdleCategroyEntity;
+
+/**
+ * @author 林俊
+ * @create 2018/3/18.
+ * @desc
+ **/
+public interface ApiIdleCategoryMapper extends  BaseDao<IdleCategroyEntity> {
+}
