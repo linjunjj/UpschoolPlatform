6d275d03d9b06da5e81e69a2d8fb5ea255fa1d5f
linjun <1045735402@qq.com>
2018-03-19 16:07:21 +0800

    公共模块的搭建 API模块  兼职模块

---
+++ mongo-api/src/main/java/com/linjun/dao/ApiPartTimeCategoryMapper.java
@@ -0,0 +1,11 @@
+package com.linjun.dao;
+
+import com.linjun.entity.PartTimeCategoryEntity;
+
+/**
+ * @author 林俊
+ * @create 2018/3/19.
+ * @desc
+ **/
+public interface ApiPartTimeCategoryMapper extends  BaseDao<PartTimeCategoryEntity>  {
+}
