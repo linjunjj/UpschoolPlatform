d5883b58bc4bb515fb12e90805834b9320d002b6
linjun <1045735402@qq.com>
2018-03-19 16:02:07 +0800

    公共模块的搭建 API模块  兼职模块

---
+++ mongo-api/src/main/java/com/linjun/dao/ApiPartTimeApplyVolumMapper.java
@@ -0,0 +1,11 @@
+package com.linjun.dao;
+
+import com.linjun.entity.PartTimeApplyVolumEntity;
+
+/**
+ * @author 林俊
+ * @create 2018/3/19.
+ * @desc
+ **/
+public interface ApiPartTimeApplyVolumMapper extends  BaseDao<PartTimeApplyVolumEntity> {
+}
