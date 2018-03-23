12cfe1811d3ab4285e1ad20f499be9826c034d25
linjun <1045735402@qq.com>
2018-03-23 11:10:18 +0800

    公共模块的搭建 api service层写

---
+++ mongo-api/src/main/java/com/linjun/service/impl/ApiAcitvityServiceImpl.java
@@ -0,0 +1,53 @@
+package com.linjun.service.impl;
+
+import com.linjun.dao.ApiActivityMapper;
+import com.linjun.entity.ActivityEntity;
+import com.linjun.service.ApiAcitvityService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 林俊
+ * @create 2018/3/23.
+ * @desc
+ **/
+public class ApiAcitvityServiceImpl implements ApiAcitvityService{
+    @Autowired
+    private ApiActivityMapper apiActivityMapper;
+    @Override
+    public ActivityEntity queryObject(Integer id) {
+        return apiActivityMapper.queryObject(id);
+    }
+
+    @Override
+    public List<ActivityEntity> queryList(Map<String, Object> map) {
+        return apiActivityMapper.queryList(map);
+    }
+
+    @Override
+    public int queryTotal(Map<String, Object> map) {
+        return apiActivityMapper.queryTotal(map);
+    }
+
+    @Override
+    public void save(ActivityEntity activityEntity) {
+         apiActivityMapper.save(activityEntity);
+    }
+
+    @Override
+    public void update(ActivityEntity activityEntity) {
+      apiActivityMapper.update(activityEntity);
+    }
+
+    @Override
+    public void delete(Integer id) {
+     apiActivityMapper.delete(id);
+    }
+
+    @Override
+    public void deleteBatch(Integer[] ids) {
+   apiActivityMapper.deleteBatch(ids);
+    }
+}
