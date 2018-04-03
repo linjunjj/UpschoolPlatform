18e904519be698422d51de3a4c37af4de85b857e
linjun <1045735402@qq.com>
2018-04-03 19:47:19 +0800

    work 逻辑层

---
+++ mongo-work/src/main/java/com/linjun/service/impl/AdPositionServiceImpl.java
@@ -0,0 +1,53 @@
+package com.linjun.service.impl;
+
+import com.linjun.dao.AdPositionMapper;
+import com.linjun.entity.AdPositionEntity;
+import com.linjun.service.AdPositionService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 林俊
+ * @create 2018/4/3.
+ * @desc
+ **/
+public class AdPositionServiceImpl implements AdPositionService {
+    @Autowired
+    private AdPositionMapper adPositionMapper;
+    @Override
+    public AdPositionEntity queryObject(Long id) {
+        return adPositionMapper.queryObject(id);
+    }
+
+    @Override
+    public List<AdPositionEntity> queryList(Map<String, Object> map) {
+        return adPositionMapper.queryList(map);
+    }
+
+    @Override
+    public int queryTotal(Map<String, Object> map) {
+        return adPositionMapper.queryTotal(map);
+    }
+
+    @Override
+    public void save(AdPositionEntity adPositionEntity) {
+   adPositionMapper.save(adPositionEntity);
+    }
+
+    @Override
+    public void update(AdPositionEntity adPositionEntity) {
+  adPositionMapper.update(adPositionEntity);
+    }
+
+    @Override
+    public void delete(Long id) {
+  adPositionMapper.delete(id);
+    }
+
+    @Override
+    public void deleteBatch(Long[] ids) {
+  adPositionMapper.deleteBatch(ids);
+    }
+}
---
+++ mongo-work/src/main/java/com/linjun/service/impl/AddressMangerServiceImpl.java
@@ -0,0 +1,53 @@
+package com.linjun.service.impl;
+
+import com.linjun.dao.AddressMangerMapper;
+import com.linjun.entity.AddressManger;
+import com.linjun.service.AddressMangerService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 林俊
+ * @create 2018/4/3.
+ * @desc
+ **/
+public class AddressMangerServiceImpl implements AddressMangerService {
+    @Autowired
+    private AddressMangerMapper addressMangerMapper;
+    @Override
+    public AddressManger queryObject(Long id) {
+        return addressMangerMapper.queryObject(id);
+    }
+
+    @Override
+    public List<AddressManger> queryList(Map<String, Object> map) {
+        return addressMangerMapper.queryList(map);
+    }
+
+    @Override
+    public int queryTotal(Map<String, Object> map) {
+        return addressMangerMapper.queryTotal(map);
+    }
+
+    @Override
+    public void save(AddressManger addressManger) {
+  addressMangerMapper.save(addressManger);
+    }
+
+    @Override
+    public void update(AddressManger addressManger) {
+addressMangerMapper.update(addressManger);
+    }
+
+    @Override
+    public void delete(Long id) {
+addressMangerMapper.delete(id);
+    }
+
+    @Override
+    public void deleteBatch(Long[] ids) {
+  addressMangerMapper.deleteBatch(ids);
+    }
+}
