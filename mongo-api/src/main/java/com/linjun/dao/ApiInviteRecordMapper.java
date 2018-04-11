4961ae66957572e1370d469cfbb78fe8a46dcae8
linjun <1045735402@qq.com>
2018-04-11 09:01:53 +0800

    invite_code 新增一个code字段

---
+++ mongo-api/src/main/java/com/linjun/dao/ApiInviteRecordMapper.java
@@ -0,0 +1,11 @@
+package com.linjun.dao;
+
+import com.linjun.entity.InviteRecordVo;
+
+/**
+ * @author 林俊
+ * @create 2018/4/11.
+ * @desc
+ **/
+public interface ApiInviteRecordMapper extends  BaseDao<InviteRecordVo> {
+}
---
+++ mongo-api/src/main/java/com/linjun/entity/InviteRecordVo.java
@@ -0,0 +1,71 @@
+package com.linjun.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author 林俊
+ * @create 2018/4/11.
+ * @desc
+ **/
+public class InviteRecordVo implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private  Long id;
+    private  Long userId;
+    private  String username;
+    private  Long isUserId;
+    private   String IsUserName;
+    private Date add_time;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getIsUserId() {
+        return isUserId;
+    }
+
+    public void setIsUserId(Long isUserId) {
+        this.isUserId = isUserId;
+    }
+
+    public String getIsUserName() {
+        return IsUserName;
+    }
+
+    public void setIsUserName(String isUserName) {
+        IsUserName = isUserName;
+    }
+
+    public Date getAdd_time() {
+        return add_time;
+    }
+
+    public void setAdd_time(Date add_time) {
+        this.add_time = add_time;
+    }
+}
