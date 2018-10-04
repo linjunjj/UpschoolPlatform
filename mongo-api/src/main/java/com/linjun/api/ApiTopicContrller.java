b3b1e663e81049fd77e059898e1a0decfdaa1114
“linjunjj” <1045735402@qq.com>
2018-10-04 15:29:43 +0800

    新增首页数据接口

--- mongo-api/src/main/java/com/linjun/api/ApiTopicContrller.java
+++ mongo-api/src/main/java/com/linjun/api/ApiTopicContrller.java
@@ -2,6 +2,7 @@ package com.linjun.api;
 
 import com.linjun.util.ApiBaseAction;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
